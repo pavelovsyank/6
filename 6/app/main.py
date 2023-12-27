2313466051a16d3059f120d272d9797b497c1a56
pavel <p@b.ru>
2023-12-27 11:19:45 +0200

    fjsdkkf

---
+++ 6/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+@app.get("/produkt/{item_id}")
+async def read_item(item_id):
+    return {"produkt_id": item_id}
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
